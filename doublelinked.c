1d2d569e0d7410b246c6128bf7113b350bf21fdf
Rich747 <101540140+rich-747@users.noreply.github.com>
2022-05-30 16:13:35 +0000

    output

--- doublelinked.c
+++ doublelinked.c
@@ -241,3 +241,18 @@ void search()
         }  
     }          
 }  
+Output:
+Choose one option from the following list ...
+
+===============================================
+
+1.Insert in Beginning
+2.Insert at last
+3.Delete from Beginning
+4.Delete from last
+5.Search
+6.Show
+7.Exit
+
+Enter your choice?
+Insert in the beginning

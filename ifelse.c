7fe94c5ed0fd14b7e238c057f4f292fe69b039fb
Rich747 <101540140+rich-747@users.noreply.github.com>
2022-05-30 16:22:55 +0000

    output

--- ifelse.c
+++ ifelse.c
@@ -2,14 +2,14 @@
 
 int main()
 {
-    printf("\n\n\t\tStudytonight - Best place to learn\n\n\n");
+   
 
     int number;
     printf("Please enter a number:\n");
     scanf("%d",&number);
-    /*
-        For single statements we can skip the curly brackets
-    */
+    
+
+    
     if(number < 100)
         printf("Number is less than 100!\n");
     else if(number == 100)
@@ -17,7 +17,9 @@ int main()
     else
         printf("Number is greater than 100!\n");
 
-    printf("\n\n\t\t\tCoding is Fun !\n\n\n");
-    
+  
     return 0;
-}
+  }
+Output:Please enter a number:
+5
+Number is less than 100!
